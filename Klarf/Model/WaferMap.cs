12dd376c8b9cdedf8bb0aec51a471c8ea2430c04
wonyejo <141125658+wonyejo@users.noreply.github.com>
2023-08-21 17:20:56 +0900

    초기 커밋.

---
+++ Klarf/Model/WaferMap.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows.Media.Imaging;
+
+using System.Windows;
+using System.Windows.Media;
+
+namespace Klarf.Model
+{
+    class WaferMap
+    {
+        public BitmapImage LoadWaferMap(string waferImgSrc)
+        {
+            // TIF 파일을 읽어서 Bitmap 형식으로 반환
+            BitmapImage WaferImg = new BitmapImage(new Uri(waferImgSrc, UriKind.RelativeOrAbsolute));
+            return WaferImg;
+        }
+      
+
+        private BitmapSource VisualizeWaferMap(DieInfo[] dieInfos, BitmapImage baseImage)
+        {
+            DrawingVisual drawingVisual = new DrawingVisual();
+            using (DrawingContext drawingContext = drawingVisual.RenderOpen())
+            {
+                foreach (DieInfo dieInfo in dieInfos)
+                {
+                    Rect rect = new Rect(dieInfo.X, dieInfo.Y, 10, 10);
+                    drawingContext.DrawRectangle(new SolidColorBrush(dieInfo.StatusColor), null, rect);
+                }
+            }
+
+            RenderTargetBitmap targetBitmap = new RenderTargetBitmap(
+                (int)baseImage.Width, (int)baseImage.Height,
+                baseImage.PixelWidth, baseImage.PixelHeight,
+                PixelFormats.Pbgra32);
+
+            targetBitmap.Render(drawingVisual);
+            return targetBitmap;
+        }
+        public class DieInfo
+        {
+            public double X { get; set; }
+            public double Y { get; set; }
+            public Color StatusColor { get; set; }
+        }
+
+
+    }
+}
---
+++ Klarf/ViewModel/WaferMapViewerVM.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using System.ComponentModel;
+
+namespace Klarf.ViewModel
+{
+    class WaferMapViewerVM : INotifyPropertyChanged
+    {
+
+        #region 필드
+        private string waferImgSrc;
+
+        public string WaferImgSrc
+        {
+            get { return waferImgSrc; }
+            set
+            {
+                waferImgSrc = value;
+                OnPropertyChanged(nameof(WaferImgSrc));
+            }
+        }
+        #endregion
+
+        #region 속성
+   
+         #endregion
+
+        #region 생성자
+        public WaferMapViewerVM()
+        {
+           
+
+        }
+        #endregion
+
+        #region 메서드
+    
+
+       
+
+     
+    }
+
+    #endregion
+
+    #region 중첩된 클래스
+
+    public abstract class INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    #endregion
+
+}
+
